e3ce7e337724b095e881083593ed8262fff54959
Cristal <camacri2@alumni.uv.es>
2022-06-08 11:30:24 +0200

    feat(back): add DB

--- tutorial/src/main/resources/data.sql
+++ tutorial/src/main/resources/data.sql
@@ -1,1 +1,3 @@
-select 1 from dual;
+INSERT INTO CATEGORY(id, name) VALUES (1, 'Eurogames');
+INSERT INTO CATEGORY(id, name) VALUES (2, 'Ameritrash');
+INSERT INTO CATEGORY(id, name) VALUES (3, 'Familiar');
