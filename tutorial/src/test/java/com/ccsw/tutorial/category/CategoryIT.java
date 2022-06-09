de45e544168d948ece7f101b6e7e3752e19b12d4
Cristal <camacri2@alumni.uv.es>
2022-06-09 11:13:07 +0200

    test(back): add tests

--- tutorial/src/test/java/com/ccsw/tutorial/category/CategoryIT.java
+++ tutorial/src/test/java/com/ccsw/tutorial/category/CategoryIT.java
@@ -7,7 +7,9 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.annotation.DirtiesContext;
 
@@ -25,6 +27,11 @@ public class CategoryIT {
     public static final String LOCALHOST = "http://localhost:";
     public static final String SERVICE_PATH = "/category/";
 
+    public static final Long NEW_CATEGORY_ID = 4L;
+    public static final String NEW_CATEGORY_NAME = "CAT4";
+    public static final Long MODIFY_CATEGORY_ID = 3L;
+    public static final Long DELETE_CATEGORY_ID = 2L;
+
     @LocalServerPort
     private int port;
 
@@ -34,12 +41,90 @@ public class CategoryIT {
     ParameterizedTypeReference<List<CategoryDto>> responseType = new ParameterizedTypeReference<List<CategoryDto>>() {
     };
 
-    @Test
+    @Test // that when calling findAll() it returns every category
     public void findAllShouldReturnAllCategories() {
         ResponseEntity<List<CategoryDto>> response = restTemplate.exchange(LOCALHOST + port + SERVICE_PATH,
                 HttpMethod.GET, null, responseType);
 
         assertNotNull(response);
         assertEquals(3, response.getBody().size());
+    }
+
+    @Test // that when saving without ID, a new category is inserted
+    public void saveWithoudIdShouldCreateNewCategory() {
+        CategoryDto dto = new CategoryDto();
+        dto.setName(NEW_CATEGORY_NAME);
+
+        // Add the dto (without id)
+        restTemplate.exchange(LOCALHOST + port + SERVICE_PATH, HttpMethod.PUT, new HttpEntity<>(dto), Void.class);
+
+        // Get the new list of categories
+        ResponseEntity<List<CategoryDto>> response = restTemplate.exchange(LOCALHOST + port + SERVICE_PATH,
+                HttpMethod.GET, null, responseType);
+        assertNotNull(response); // Check if not null
+        assertEquals(4, response.getBody().size()); // Check if new size is correct
+
+        // Search for the new item and get its ID. Check if corresponds with reality
+        CategoryDto categorySearch = response.getBody().stream().filter(item -> item.getId().equals(NEW_CATEGORY_ID))
+                .findFirst().orElse(null);
+        assertNotNull(categorySearch); // Check if not null
+        assertEquals(NEW_CATEGORY_NAME, categorySearch.getName()); // Check if result name is the same as new name
+    }
+
+    @Test // that when saving with ID, the category is modified
+    public void modifyWithExistIdShouldModifyCategory() {
+        CategoryDto dto = new CategoryDto();
+        dto.setName(NEW_CATEGORY_NAME);
+
+        // Call save with ID
+        restTemplate.exchange(LOCALHOST + port + SERVICE_PATH + MODIFY_CATEGORY_ID, HttpMethod.PUT,
+                new HttpEntity<>(dto), Void.class);
+
+        // Get the list of categories
+        ResponseEntity<List<CategoryDto>> response = restTemplate.exchange(LOCALHOST + port + SERVICE_PATH,
+                HttpMethod.GET, null, responseType);
+        assertNotNull(response); // Check if not null
+        assertEquals(3, response.getBody().size()); // Check if size is the same
+
+        // Get the modified category
+        CategoryDto categorySearch = response.getBody().stream().filter(item -> item.getId().equals(MODIFY_CATEGORY_ID))
+                .findFirst().orElse(null);
+        assertNotNull(categorySearch); // Check if not null
+        assertEquals(NEW_CATEGORY_NAME, categorySearch.getName()); // Check if names are the same
+    }
+
+    @Test // that when saving with wrong ID, it returns an error
+    public void modifyWithNotExistIdShouldInternalError() {
+        CategoryDto dto = new CategoryDto();
+        dto.setName(NEW_CATEGORY_NAME);
+
+        // Call save with non-existing ID
+        ResponseEntity<?> response = restTemplate.exchange(LOCALHOST + port + SERVICE_PATH + NEW_CATEGORY_ID,
+                HttpMethod.PUT, new HttpEntity<>(dto), Void.class);
+
+        // Check that the response was an internal server error
+        assertEquals(HttpStatus.INTERNAL_SERVER_ERROR, response.getStatusCode());
+    }
+
+    @Test // that when deleting a category, it deletes the category
+    public void deleteWithExistsIdShouldDeleteCategory() {
+        // Call delete with ID
+        restTemplate.exchange(LOCALHOST + port + SERVICE_PATH + DELETE_CATEGORY_ID, HttpMethod.DELETE, null,
+                Void.class);
+
+        // Get list of categories
+        ResponseEntity<List<CategoryDto>> response = restTemplate.exchange(LOCALHOST + port + SERVICE_PATH,
+                HttpMethod.GET, null, responseType);
+        assertNotNull(response); // Check if not null
+        assertEquals(2, response.getBody().size()); // Check if size is one less
+    }
+
+    @Test // that when deleting a non-existing category, it returns an error
+    public void deleteWithNoExistsIdShouldInternalError() {
+        // Call delete with non-existing ID
+        ResponseEntity<?> response = restTemplate.exchange(LOCALHOST + port + SERVICE_PATH + NEW_CATEGORY_ID,
+                HttpMethod.DELETE, null, Void.class);
+        // Check that the response was an internal server error
+        assertEquals(HttpStatus.INTERNAL_SERVER_ERROR, response.getStatusCode());
     }
 }
--- tutorial/src/test/java/com/ccsw/tutorial/category/CategoryTest.java
+++ tutorial/src/test/java/com/ccsw/tutorial/category/CategoryTest.java
@@ -4,12 +4,14 @@ import com.ccsw.tutorial.category.model.Category;
 import com.ccsw.tutorial.category.model.CategoryDto;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
@@ -19,13 +21,16 @@ import static org.mockito.Mockito.*;
  */
 @ExtendWith(MockitoExtension.class)
 public class CategoryTest {
+    public static final String CATEGORY_NAME = "CAT1";
+    public static final Long EXISTS_CATEGORY_ID = 1L;
+
     @Mock
     private CategoryRepository categoryRepository;
 
     @InjectMocks
     private CategoryServiceImpl categoryService;
 
-    @Test
+    @Test // that when calling findAll() it returns every category
     public void findAllShouldReturnAllCategories() {
         List<Category> list = new ArrayList<>();
         list.add(mock(Category.class));
@@ -36,5 +41,47 @@ public class CategoryTest {
 
         assertNotNull(categories);
         assertEquals(1, categories.size());
+    }
+
+    @Test // that when saving without ID, a new category is inserted
+    public void saveNotExistsCategoryIdShouldInsert() {
+        CategoryDto categoryDto = new CategoryDto();
+        categoryDto.setName(CATEGORY_NAME);
+
+        ArgumentCaptor<Category> category = ArgumentCaptor.forClass(Category.class);
+
+        // Call save with null ID (non-existent category)
+        categoryService.save(null, categoryDto);
+
+        // Simulate save action
+        verify(categoryRepository).save(category.capture());
+
+        // Check if value is the provided one
+        assertEquals(CATEGORY_NAME, category.getValue().getName());
+    }
+
+    @Test // that when saving with ID, the category is modified
+    public void saveExistsCategoryIdShouldUpdate() {
+        CategoryDto categoryDto = new CategoryDto();
+        categoryDto.setName(CATEGORY_NAME);
+
+        // Create mock category
+        Category category = mock(Category.class);
+        // Find an existing category (id 1)
+        when(categoryRepository.findById(EXISTS_CATEGORY_ID)).thenReturn(Optional.of(category));
+
+        // Call save with existing ID and mock category
+        categoryService.save(EXISTS_CATEGORY_ID, categoryDto);
+
+        // Verify that save was called over the same category
+        verify(categoryRepository).save(category);
+    }
+
+    @Test // that when deleting a category, it deletes the category
+    public void deleteExistsCategoryIdShouldDelete() {
+        // Delete the category
+        categoryService.delete(EXISTS_CATEGORY_ID);
+        // Verify that the category was deleted
+        verify(categoryRepository).deleteById(EXISTS_CATEGORY_ID);
     }
 }
